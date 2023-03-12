d321cba91afcd88884f898ed30ef8c1e73885c77
Nowshin7033 <nowshin5160@gmail.com>
2023-03-13 02:27:30 +0600

    first commit

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div  className='h-screen flex justify-center items-center'>
+            <h2 className='text-3xl'>Comming Soon</h2>
+        </div>
+    );
+}; 
+
+export default About;
---
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import AvailableAppointments from '../AvailableAppointments/AvailableAppointments';
+
+const Appointment = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    return (
+        <div className='h-screen flex justify-center items-center'>
+            {/* <AppointmentBanner
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+            ></AppointmentBanner>
+            <AvailableAppointments
+                selectedDate={selectedDate}
+            ></AvailableAppointments> */}
+            <h2 className='text-3xl'>Comming Soon</h2>
+        </div>
+    );
+};
+
+export default Appointment;
